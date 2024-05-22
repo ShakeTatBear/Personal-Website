1085bcd073f1988caff87d20af7e82494382b3d7
Josh Brown <joshalbrown@gmail.com>
2024-05-22 16:35:45 -0400

    General Update
    Small changes 5/22

--- react-app/src/Services/services.jsx
+++ react-app/src/Services/services.jsx
@@ -46,22 +46,29 @@ const ServicesSection = () => {
         
         <ServiceCard 
           icon={faMobileAlt}
-          title="Coding & App Development"
-          description="Crafting seamless and intuitive applications for all platforms. Building robust and scalable software solutions with clean and efficient code."
+          title="3D Modelling"
+          description="Whether for games, simulations, or product design, my models are detailed, realistic, and optimized for performance, providing a solid foundation for your visual projects."
           link="https://github.com/ShakeTatBear"
         />
 
         <ServiceCard 
           icon={faMobileAlt}
-          title="Coding & App Development"
-          description="Crafting seamless and intuitive applications for all platforms. Building robust and scalable software solutions with clean and efficient code."
+          title=" Database Management & SQL"
+          description="Efficient data management is crucial. I specialize in SQL database design, implementation, and maintenance, ensuring any data is organized, secure, and easily accessible for seamless operations."
           link="https://github.com/ShakeTatBear"
         />
 
         <ServiceCard 
           icon={faMobileAlt}
-          title="Coding & App Development"
-          description="Crafting seamless and intuitive applications for all platforms. Building robust and scalable software solutions with clean and efficient code."
+          title="UX/UI Design"
+          description="User experience and interface design are key to engaging and retaining users. My UX/UI design services focus on creating intuitive, visually appealing, and accessible interfaces that enhance user satisfaction and drive engagement."
+          link="https://github.com/ShakeTatBear"
+        />
+
+        <ServiceCard 
+          icon={faMobileAlt}
+          title="Networking"
+          description="Ensure reliable and secure connectivity with our networking and server setup services. We design, configure, and maintain network infrastructure and servers, providing robust solutions tailored to your needs for optimal performance and scalability."
           link="https://github.com/ShakeTatBear"
         />
         
